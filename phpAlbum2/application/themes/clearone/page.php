ee55f83d6710215ffed555d8af25bd8f94026278
patrik.jakab <patrik.jakab@75702ad8-2a71-11de-979f-8d47a8973ba4>
2012-12-15 07:56:42 +0000

    little change on page.php

--- phpAlbum2/application/themes/clearone/page.php
+++ phpAlbum2/application/themes/clearone/page.php
@@ -2,18 +2,19 @@
 <?php if($head){ echo $head; } ?>
 <body>
 <table>
-<?php if($header){ ?>
-    <tr><td>
-<?php echo $header; ?>
-    </td></tr>
-<?php } ?>
-    <tr>
-<?php
+<?php 
+    if($header){
+    	echo "<tr><td>$header</td></tr>";
+	}
+	echo "<tr>";	
     if($left) echo "<td id=\"left\">$left</td>";
     if($content) echo "<td id=\"content\">$content</td>";
     if($right) echo "<td id=\"right\">$right</td>";
+    echo "</tr>";
+    if($footer){
+    	echo "<tr><td>$header</td></tr>";
+	}
 ?>
-</tr>
 </table>
 </body>
 </html>
